c261cc8b8ad3a8bfa49839850b79160d6aab5163
Maximiliano Ayala <max@youthwar.com>
2020-12-03 17:49:16 -0800

    test this

--- ci/sync-release.js
+++ ci/sync-release.js
@@ -9,38 +9,59 @@ const syncRelease = async () => {
   const octokit = new Octokit({
     auth: GITHUB_TOKEN,
   });
-  console.log(COMMIT)
-  const commit = await octokit.git.getCommit({
+
+  const { data: commit } = await octokit.git.getCommit({
     owner,
     repo,
     commit_sha: COMMIT,
   });
 
   const { data } = await octokit.search.users({
-    q: `${commit.data.author.email} in:email`
+    q: `${commit.author.email} in:email`
   });
 
   const [ user ] = data.items;
 
   console.log({ user });
-  
-  const result = await octokit.pulls.create({
-    owner,
-    repo,
-    head: 'master',
-    base: 'release',
-    maintainer_can_modify: true,
-    title: 'Test',
-    draft: false,
-    body: `
-      @${user.login} has committed this branch to master with a label of release.
-      The original commit can be found [here](https://github.com/${owner}/${repo}/commit/${COMMIT})
-    `
-  });
+
+  const refName = COMMIT.substr(0,7);
+  let createdRef;
+
+  try {
+    const { data } = await octokit.git.createRef({
+      owner,
+      repo,
+      ref: `refs/heads/master-release-sync-${refName}`,
+      sha: COMMIT,
+    });
+    createdRef = data.ref;
+  } catch (e) {
+    
+  }
+
+  console.log(createdRef);
+  try {
+    const result = await octokit.pulls.create({
+      owner,
+      repo,
+      head: createdRef.ref,
+      base: 'release',
+      maintainer_can_modify: true,
+      title: 'Test',
+      draft: false,
+      body: `
+        @${user.login} has committed this branch to master with a label of release.
+        The original commit can be found [here](https://github.com/${owner}/${repo}/commit/${COMMIT})
+      `
+    });
+  } catch (e) {
+    console.log(e);
+
+  }
   
   
   console.log({ result })
-   
+  
 };
 
 syncRelease();
